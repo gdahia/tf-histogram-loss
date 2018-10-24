412409c8942d78bd8f21373f390b3efd12bbe3db
Gabriel Dahia <gdahia@gmail.com>
2018-10-24 11:46:51 -0300

    Add docstring to utils.flat_strict_upper

--- histogram_loss/utils.py
+++ histogram_loss/utils.py
@@ -2,7 +2,13 @@ import tensorflow as tf
 
 
 def flat_strict_upper(mat: tf.Tensor) -> tf.Tensor:
-  """
+  """Converts the strictly upper triangular part of a matrix into an array.
+
+  Args:
+    mat: a `tf.Tensor` matrix.
+
+  Returns:
+    The strictly upper triangular part of `mat` in a `tf.Tensor` array.
   """
   with tf.name_scope('flat_strict_upper'):
     mask = tf.ones_like(mat, dtype=tf.int32)
