8b3da5f3da72178ae22bb75ec532a73dd63e6c2b
Gabriel Dahia <gdahia@gmail.com>
2018-10-24 11:47:27 -0300

    Replace tf.boolean_mask with tf.dynamic_partition
    TensorFlow issues a warning of heavy memory usage when using
    tf.boolean_mask and gradients. Issue, and adopted solution, can be found
    here https://stackoverflow.com/questions/44380727/get-userwarning-while-i-use-tf-boolean-mask

--- histogram_loss/utils.py
+++ histogram_loss/utils.py
@@ -11,8 +11,7 @@ def flat_strict_upper(mat: tf.Tensor) -> tf.Tensor:
     The strictly upper triangular part of `mat` in a `tf.Tensor` array.
   """
   with tf.name_scope('flat_strict_upper'):
-    mask = tf.ones_like(mat, dtype=tf.int32)
-    eye = tf.matrix_band_part(mask, 0, 0)
-    mask = tf.matrix_band_part(mask, 0, -1) - eye
-    mask = tf.cast(mask, dtype=tf.bool)
-    return tf.boolean_mask(mat, mask)
+    partitions = tf.ones_like(mat, dtype=tf.int32)
+    eye = tf.matrix_band_part(partitions, 0, 0)
+    partitions = tf.matrix_band_part(partitions, 0, -1) - eye
+    return tf.dynamic_partition(mat, partitions, 2)[1]
