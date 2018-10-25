217dfc6ec0a6d2892ed24e5a9c4a0e1edcdebe1b
Gabriel Dahia <gdahia@gmail.com>
2018-10-25 08:37:20 -0300

    Improve utils.flat_strict_upper tests
    Removed specific tests, i.e. the transposition ones, and improve the
    verification ones in terms of cases covered and runtime.

--- histogram_loss/tests/utils_test.py
+++ histogram_loss/tests/utils_test.py
@@ -3,29 +3,39 @@ from tensorflow.python.eager.context import eager_mode
 import numpy as np
 
 import pytest
-import hypothesis
-from hypothesis import given, assume
+from hypothesis import given
 import hypothesis.strategies as st
 import hypothesis.extra.numpy as np_st
 
 from histogram_loss import utils
 
 
+@pytest.fixture(scope='function')
+def float_graph():
+  input_pl = tf.placeholder(tf.float32)
+  output = utils.flat_strict_upper(input_pl)
+  yield input_pl, output
+
+
+@pytest.fixture(scope='function')
+def int_graph():
+  input_pl = tf.placeholder(tf.int64)
+  output = utils.flat_strict_upper(input_pl)
+  yield input_pl, output
+
+
 class TestUtils:
   @given(st.data())
-  def test_eager_flat_strict_upper_oracle(self, data):
+  def test_eager_flat_strict_upper_int_verification(self, data):
     with eager_mode():
       # sample square matrix
       n = data.draw(st.integers(min_value=3, max_value=10))
       input_ = data.draw(
-          np_st.arrays(dtype=np.int32, shape=(n, n), unique=True))
+          np_st.arrays(dtype=np_st.integer_dtypes(), shape=(n, n)))
 
-      # dont use nan test cases
-      assume(not np.any(np.isnan(input_)))
+      output = np.array(utils.flat_strict_upper(input_))
 
-      output = utils.flat_strict_upper(input_).numpy()
-
-      # check manually
+      # verify
       k = 0
       for i in range(n):
         for j in range(i + 1, n):
@@ -33,61 +43,99 @@ class TestUtils:
           k += 1
 
   @given(st.data())
-  def test_eager_flat_strict_upper_unique_transposition_different(self, data):
+  def test_eager_flat_strict_upper_float_verification(self, data):
     with eager_mode():
       # sample square matrix
       n = data.draw(st.integers(min_value=3, max_value=10))
-      input_ = data.draw(np_st.arrays(dtype=np.int32, shape=(n, n)))
+      elements = st.floats(allow_nan=False, allow_infinity=False, width=32)
+      input_ = data.draw(
+          np_st.arrays(dtype=np.float32, elements=elements, shape=(n, n)))
 
-      # dont use symmetrical matrix test cases
-      assume(not np.all(input_ == input_.T))
-
-      # compute for inputs and its transpose
       output = utils.flat_strict_upper(input_).numpy()
-      output_T = utils.flat_strict_upper(input_.T).numpy()
 
-      with pytest.raises(AssertionError):
-        np.testing.assert_array_equal(output_T, output)
+      # verify
+      k = 0
+      for i in range(n):
+        for j in range(i + 1, n):
+          assert output[k] == input_[i, j]
+          k += 1
 
-  @given(st.data())
-  def test_eager_flat_strict_upper_symmetric_invariance_to_transposition(
-      self, data):
-    # remove filter too much health check
-    hypothesis.settings(
-        suppress_health_check=[hypothesis.HealthCheck.filter_too_much])
-
-    with eager_mode():
-      # sample square matrix
-      n = data.draw(st.integers(min_value=3, max_value=10))
-      input_ = data.draw(np_st.arrays(dtype=np.int32, shape=(n, n)))
-
-      # use only symmetrical matrix test cases
-      assume(np.all(input_ == input_.T))
-
-      # compute for inputs and its transpose
-      output = utils.flat_strict_upper(input_).numpy()
-      output_T = utils.flat_strict_upper(input_.T).numpy()
-
-      np.testing.assert_array_equal(output_T, output)
-
-  @given(st.data())
-  def test_flat_strict_upper_symmetric_invariance_to_transposition(self, data):
-    # remove filter too much health check
-    hypothesis.settings(
-        suppress_health_check=[hypothesis.HealthCheck.filter_too_much])
-
+  @given(data=st.data(), dtype=np_st.integer_dtypes())
+  def test_flat_strict_upper_int_verification(self, int_graph, data, dtype):
     # sample square matrix
     n = data.draw(st.integers(min_value=3, max_value=10))
-    input_ = data.draw(np_st.arrays(dtype=np.int32, shape=(n, n)))
+    input_ = data.draw(np_st.arrays(dtype=dtype, shape=(n, n)))
 
-    # use only symmetrical matrix test cases
-    assume(np.all(input_ == input_.T))
+    # graph building
+    input_pl, output = int_graph
 
-    # compute for inputs and its transpose
-    input_pl = tf.placeholder(tf.int32)
-    output = utils.flat_strict_upper(input_pl)
-
+    # running
     with tf.Session() as sess:
       output_val = sess.run(output, feed_dict={input_pl: input_})
-      output_T_val = sess.run(output, feed_dict={input_pl: input_.T})
-      np.testing.assert_array_equal(output_T_val, output_val)
+
+    # verify
+    k = 0
+    for i in range(n):
+      for j in range(i + 1, n):
+        assert output_val[k] == input_[i, j]
+        k += 1
+
+  @given(data=st.data())
+  def test_flat_strict_upper_float_verification(self, float_graph, data):
+    # sample square matrix
+    n = data.draw(st.integers(min_value=3, max_value=10))
+    elements = st.floats(allow_nan=False, allow_infinity=False, width=32)
+    input_ = data.draw(
+        np_st.arrays(dtype=np.float32, elements=elements, shape=(n, n)))
+
+    # graph building
+    input_pl, output = float_graph
+
+    # running
+    with tf.Session() as sess:
+      output_val = sess.run(output, feed_dict={input_pl: input_})
+
+    # verify
+    k = 0
+    for i in range(n):
+      for j in range(i + 1, n):
+        assert output_val[k] == input_[i, j]
+        k += 1
+
+  @given(data=st.data(), dtype=np_st.integer_dtypes())
+  def test_flat_strict_upper_int_eager_graph_consistency(
+      self, int_graph, data, dtype):
+    # sample square matrix
+    n = data.draw(st.integers(min_value=3, max_value=10))
+    input_ = data.draw(np_st.arrays(dtype=dtype, shape=(n, n)))
+
+    # compute eager output
+    with eager_mode():
+      eager_output = utils.flat_strict_upper(input_)
+
+    # compute graph output
+    input_pl, output = int_graph
+    with tf.Session() as sess:
+      graph_output = sess.run(output, feed_dict={input_pl: input_})
+
+    np.testing.assert_equal(eager_output, graph_output)
+
+  @given(data=st.data())
+  def test_flat_strict_upper_float_eager_graph_consistency(
+      self, float_graph, data):
+    # sample square matrix
+    n = data.draw(st.integers(min_value=3, max_value=10))
+    elements = st.floats(allow_nan=False, allow_infinity=False, width=32)
+    input_ = data.draw(
+        np_st.arrays(dtype=np.float32, elements=elements, shape=(n, n)))
+
+    # compute eager output
+    with eager_mode():
+      eager_output = utils.flat_strict_upper(input_)
+
+    # compute graph output
+    input_pl, output = float_graph
+    with tf.Session() as sess:
+      graph_output = sess.run(output, feed_dict={input_pl: input_})
+
+    np.testing.assert_equal(eager_output, graph_output)
