76f3a69b545e02682ab2fee8a47ea4fa7c629abe
Gabriel Dahia <gdahia@gmail.com>
2018-10-28 18:31:36 -0300

    Add layers.InceptionResNetBlock base class

--- histogram_loss/layers.py
+++ histogram_loss/layers.py
@@ -1,12 +1,75 @@
 from typing import List, Dict, Any
+from abc import ABC
 
 import tensorflow as tf
 
 
-class Block35(tf.keras.layers.Layer):
+class InceptionResNetBlock(tf.keras.layers.Layer, ABC):
+  """Base Inception ResNet block keras layer."""
+
+  def build(self, input_shape: tf.TensorShape) -> None:
+    # build each layer of each branch according
+    # to the output shape of the previous layer
+    for branch in self._branches:
+      branch_input_shape = input_shape
+      for layer in branch:
+        layer.build(branch_input_shape)
+        branch_input_shape = layer.compute_output_shape(branch_input_shape)
+
+    # build up
+    self._up = tf.keras.layers.Conv2D(
+        filters=input_shape[3],
+        kernel_size=1,
+        strides=1,
+        padding='same',
+        activation=None)
+    self._up.build(input_shape)
+
+    super(InceptionResNetBlock, self).build(input_shape)
+
+  def call(self, inputs: tf.Tensor) -> tf.Tensor:
+    # forward through branches
+    branch_outputs = []
+    for branch in self._branches:
+      branch_output = inputs
+      for layer in branch:
+        branch_output = layer.call(branch_output)
+      branch_outputs.append(branch_output)
+
+    # stack branches
+    mixed = tf.concat(branch_outputs, 3)
+    up_out = self._up.call(mixed)
+
+    # residual connection
+    inputs += self._scale * up_out
+
+    # apply activation function, if any
+    if self._activation:
+      inputs = self._activation(inputs)
+
+    return inputs
+
+  def compute_output_shape(self,
+                           input_shape: tf.TensorShape) -> tf.TensorShape:
+    shape = tf.TensorShape(input_shape).as_list()
+    shape[-1] = 128
+    return tf.TensorShape(shape)
+
+  def get_config(self) -> Dict[str, Any]:
+    base_config = super(InceptionResNetBlock, self).get_config()
+    base_config['scale'] = self._scale
+    base_config['activation'] = self._activation
+    return base_config
+
+  @classmethod
+  def from_config(cls, config: Dict[str, Any]):
+    return cls(**config)
+
+
+class Block35(InceptionResNetBlock):
   """Inception ResNet 35x35 block layer.
   
-  This layer creates a layer to encapsulate a ResNet 35x35 block.
+  This layer encapsulates a ResNet 35x35 block.
 
   Args:
     scale: Activation scaling constant.
@@ -20,7 +83,9 @@ class Block35(tf.keras.layers.Layer):
     self._scale = scale
     self._activation = activation
     self._branches = []  # type: List[List[tf.keras.layers.Layer]]
+    super(InceptionResNetBlock, self).__init__(**kwargs)
 
+  def build(self, input_shape: tf.TensorShape) -> None:
     # branch 0
     branch0 = []  # tyep: List[tf.keras.layers.Layer]
     branch0_0 = tf.keras.layers.Conv2D(
@@ -96,57 +161,5 @@ class Block35(tf.keras.layers.Layer):
     branch2.append(batch_norm)
     self._branches.append(branch2)
 
-    # up
-    self._up = tf.keras.layers.Conv2D(
-        filters=128, kernel_size=1, strides=1, padding='same', activation=None)
-
-    super(Block35, self).__init__(**kwargs)
-
-  def build(self, input_shape: tf.TensorShape) -> None:
-    # build each layer according to the
-    # output shape of the previous layer
-    for branch in self._branches:
-      branch_input_shape = input_shape
-      for layer in branch:
-        layer.build(branch_input_shape)
-        branch_input_shape = layer.compute_output_shape(branch_input_shape)
-
+    # build branches, up, and internal keras layer structure
     super(Block35, self).build(input_shape)
-
-  def call(self, inputs: tf.Tensor) -> tf.Tensor:
-    # forward through branches
-    branch_outputs = []
-    for branch in self._branches:
-      branch_output = inputs
-      for layer in branch:
-        branch_output = layer.call(branch_output)
-      branch_outputs.append(branch_output)
-
-    # stack branches
-    mixed = tf.concat(branch_outputs, 3)
-    up_out = self._up.call(mixed)
-
-    # residual connection
-    inputs += self._scale * up_out
-
-    # apply activation function, if any
-    if self._activation:
-      inputs = self._activation(inputs)
-
-    return inputs
-
-  def compute_output_shape(self,
-                           input_shape: tf.TensorShape) -> tf.TensorShape:
-    shape = tf.TensorShape(input_shape).as_list()
-    shape[-1] = 128
-    return tf.TensorShape(shape)
-
-  def get_config(self) -> Dict[str, Any]:
-    base_config = super(Block35, self).get_config()
-    base_config['scale'] = self._scale
-    base_config['activation'] = self._activation
-    return base_config
-
-  @classmethod
-  def from_config(cls, config: Dict[str, Any]):
-    return cls(**config)
